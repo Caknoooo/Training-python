1eea9fcf00f147c5e3d82d8f799db09e2ed60d4a
Caknoooo <caknocomel@gmail.com>
2022-06-27 16:06:10 +0700

    Menambahkan file index.java

---
+++ index.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class index{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+
+		String nama_barang;
+		int price, much;
+
+		System.out.print("Enter item Name : ");
+		nama_barang = input.nextLine();
+		if(nama_barang == ""){
+			System.out.println("This item not found");
+			System.exit(0);
+		}
+
+		System.out.print("Enter item price : ");
+		price = input.nextInt();
+		if(price <= 0)
+			System.out.println("Undefined price");
+
+		System.out.print("Enter the Numbers of item : ");
+		much = input.nextInt();
+		if (much < 0) {
+			System.out.println("Undefined much of items");
+		}
+
+		System.out.println("---------------------------------");
+
+		System.out.println("Name Product : " + nama_barang);
+		System.out.println("Price total : " + (price * much));
+ 
+	}
+}
